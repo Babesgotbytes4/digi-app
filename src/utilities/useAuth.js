3d11ec4bde824b0e895b297d8ee97e7785929b56
amanda <amandagxagxa@gmail.com>
2021-05-19 12:12:30 +0200

    adding my UI

---
+++ src/utilities/useAuth.js
@@ -0,0 +1,9 @@
+import{useState} from 'react';
+
+const useState = ()=> {
+    const [pin, setPin]= useState('')
+    const
+}
+
+
+export default useAuth;
--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,7 +1,86 @@
-import React from 'react';
+import React from "react";
+import PinCode from "react-pin-input";
 
-export const Register = () => {
-    return <div>Register</div>
-};
+function Register() {
+  const [pinValues, setPinValues] = React.useState(["", "", "", ""]);
+  const addUser = (props) => {
+    const { username, pin, confirmPin } = props;
+
+    if (username.length < 3) return "username is too shot";
+    if (pin.length < 4) return "pin is too short";
+    if (confirmPin !== pin) return "pin doesn't match";
+
+    const userString = window.localStorage.getItem("users");
+
+    if (!userString) {
+      // saving the name and the pin
+      window.localStorage.setItem("uses", JSON.stringify([{ username, pin }]));
+      return null;
+    }
+    const usersData = JSON.parse(userString);
+    if (usersData.find((user) => user.username === username))
+      return "user exists";
+
+    window.localStorage.setItem(
+      "uses",
+      JSON.stringify([...usersData, { username, pin }])
+    );
+  };
+  //check if the user exists
+
+  // const checkUsers = (props) => {
+  //   const { username, pin } = props;
+
+  //   if (username.length < 1) return "no- username";
+  //   if (pin < 4) return "pin is too short";
+
+  //   const userString = window.localStorage.getItem("users");
+
+  //   if (!userString) return "no user created";
+
+  //   const usersData = JSON.parse(userString);
+  //   const username = usersData.find(
+  //     (checkingUser) => checkingUser.username === username
+  //   );
+
+  //   //if no user existing
+  //   if (!username) return "user does not exist";
+  //   if (username.pin === pin) return null;
+  // };
+
+  return (
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Enter Pin:
+        <PinCode
+          disabled
+          length={4}
+          values={addUser.confirmPin}
+          onChange={({ values }) => {
+            setPinValues(values);
+          }}
+        />
+      </label>
+
+      <label>
+        Cornfim Pin:
+        <PinCode
+          disabled
+          length={4}
+          values={addUser.pinValues}
+          onChange={({ values }) => {
+            setPinValues(values);
+          }}
+        />
+      </label>
+    </form>
+  );
+}
 
 export default Register;
+
+// export default Register;
