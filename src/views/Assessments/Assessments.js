e92639c71487b5cd61b40f94635d5b2cf4c28389
Phindie <pn8256@gmail.com>
2021-05-24 11:54:09 +0200

    added new page

--- src/views/Assessments/Assessments.js
+++ src/views/Assessments/Assessments.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Button = styled.button`
+    background-color: #ccb3ff;
+    padding: 5px 70px;
+    width: 700px;
+    height:  80px;
+    margin-left: .5rem;
+    
+`
+
 
 export const Assessments = () => {
-    return <div>HELLO</div>
+    return <div><h1>Basic</h1>
+
+    <Button href={"/login"}>
+    Intro 
+  </Button>
+  <Button href={"/login"}>
+    Keyboard 
+  </Button>
+  <Button href={"/login"}>
+    Mouse + Mousepad
+  </Button>
+  <Button href={"/login"}>
+    Other 
+  </Button>
+  </div>
 };
 
 export default Assessments;
