bfd03a081582724fee49c7cb7777990daba02e9f
Phindie <pn8256@gmail.com>
2021-05-25 13:56:50 +0200

    Added assessments data

--- src/views/Assessments/Assessments.js
+++ src/views/Assessments/Assessments.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import useAuth from '../../utilities/useAuth'
+import React, { useEffect, useState } from 'react'
+// import {Button} from "@material-ui/core";
+// import useAuth from "../../utilities/useAuth";
+// import styled from "styled-components";
+// import Link from "../Link/Link";
 
-export const Assessments = () => {
-    const {handleLogOutButton}=useAuth({
-        shouldBeLoggedIn:true,
-    })
-    return (
-    
-    <div>
-        <button onClick={handleLogOutButton}>Log out</button>
-    </div>
+import cms from '../../api/cms/cms';
+import app from "../../api/app/app";
+
+
+
+
+// export const Logout = () => {
+//   const { handleLogOutButton } = useAuth({
+//     shouldBeLoggedIn: true,
+//   });
+//   return (
+//     <div>
+//       <button onClick={handleLogOutButton}>Log out</button>
+//     </div>
+//   );
+// };
+
+
+
+ function Assessments() {
+
         
-        )
+const [assessments,  setAssessments] = useState([])
+
+const checkAndSync = async () => {
+  if(app.calcIfShowIdSync()) {
+    const response = await cms.syncAssessments();
+    setAssessments(response);
+  }
+
+  const response = JSON.parse(window.localStorage.getItem("assessments"))
+  setAssessments(response);
 };
 
-export default Assessments;
+useEffect(() => checkAndSync(), []);
+
+
+return (
+  <div>
+    <div>
+      {assessments.map(({ name }) => (
+        <div>{name}</div>     
+      ))}
+    </div>
+  </div>
+);
+
+
+}
+   
+  
+
+  export default Assessments;
