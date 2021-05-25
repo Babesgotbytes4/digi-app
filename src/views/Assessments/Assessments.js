aff63d3a1d5ac5f7d062e7aa6a7789dcc21d012f
Phindie <pn8256@gmail.com>
2021-05-25 14:46:35 +0200

    assessments api

--- src/views/Assessments/Assessments.js
+++ src/views/Assessments/Assessments.js
@@ -37,21 +37,6 @@ const checkAndSync = async () => {
 
   const response = JSON.parse(window.localStorage.getItem("assessments"))
   setAssessments(response);
-=======
-import React from 'react';
-import useAuth from '../../utilities/useAuth'
-
-export const Assessments = () => {
-    const {handleLogOutButton}=useAuth({
-        shouldBeLoggedIn:true,
-    })
-    return (
-    
-    <div>
-        <button onClick={handleLogOutButton}>Log out</button>
-    </div>
-        
-        )
 
 };
 
