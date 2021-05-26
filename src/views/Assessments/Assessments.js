82ff2c466b2271a2341c42ed6ae0a7aeb8452549
Phindie <pn8256@gmail.com>
2021-05-26 10:06:18 +0200

    Added footer and assessments data

--- src/views/Assessments/Assessments.js
+++ src/views/Assessments/Assessments.js
@@ -1,61 +1,41 @@
-
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 // import {Button} from "@material-ui/core";
-// import useAuth from "../../utilities/useAuth";
+import useAuth from "../../utilities/useAuth";
 // import styled from "styled-components";
 // import Link from "../Link/Link";
 
-import cms from '../../api/cms/cms';
+import cms from "../../api/cms/cms";
 import app from "../../api/app/app";
 
+function Assessments() {
+  const { handleLogOutButton } = useAuth({
+    shouldBeLoggedIn: true,
+  });
 
+  const [assessments, setAssessments] = useState([]);
 
+  const checkAndSync = async () => {
+    if (app.calcIfShowIdSync()) {
+      const response = await cms.syncAssessments();
+      setAssessments(response);
+    }
 
-// export const Logout = () => {
-//   const { handleLogOutButton } = useAuth({
-//     shouldBeLoggedIn: true,
-//   });
-//   return (
-//     <div>
-//       <button onClick={handleLogOutButton}>Log out</button>
-//     </div>
-//   );
-// };
-
-
-
- function Assessments() {
-
-        
-const [assessments,  setAssessments] = useState([])
-
-const checkAndSync = async () => {
-  if(app.calcIfShowIdSync()) {
-    const response = await cms.syncAssessments();
+    const response = JSON.parse(window.localStorage.getItem("assessments"));
     setAssessments(response);
-  }
+  };
 
-  const response = JSON.parse(window.localStorage.getItem("assessments"))
-  setAssessments(response);
+  useEffect(() => checkAndSync(), []);
 
-};
-
-useEffect(() => checkAndSync(), []);
-
-
-return (
-  <div>
+  return (
     <div>
-      {assessments.map(({ name }) => (
-        <div>{name}</div>     
-      ))}
+      <div>
+        <button onClick={handleLogOutButton}>Log out</button>
+        {assessments.map(({ name }) => (
+          <div>{name}</div>
+        ))}
+      </div>
     </div>
-  </div>
-);
-
-
+  );
 }
-   
-  
 
-  export default Assessments;
+export default Assessments;
