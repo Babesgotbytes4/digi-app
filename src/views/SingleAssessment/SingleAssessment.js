1f173ef9ae480f1f0c7170b99f15348c42d1b86d
phelisa2020 <phelisagqwetha@gmail.com>
2021-06-19 16:12:23 +0200

    pulling about

--- src/views/SingleAssessment/SingleAssessment.js
+++ src/views/SingleAssessment/SingleAssessment.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { Button, Accordion ,FormLabel} from "@material-ui/core";
+// import { Button, Accordion ,FormLabel} from "@material-ui/core";
 import styled from "styled-components";
-import { QuestionAnswer } from "@material-ui/icons";
+// import { QuestionAnswer } from "@material-ui/icons";
+// import Smilly from '../Smilly'
 // import Assessments from "../Assessments"
 
 const List = styled.div`
@@ -22,31 +23,41 @@ const getAssessment = (assessmentId) => {
   const results = assessmentsData.find(
     (assessment) => assessment.id === assessmentId
   );
-  console.log(results);
   return results;
 };
 
 const SingleAssessment = () => {
   const [question, setQuestion] = useState(1)
+  const [answer, setAnswer] = useState(1)
   const { assessmentId } = useParams();
   const [singleAssessment, setSingleAssessment] = useState(
     getAssessment(assessmentId)
-  );
+  )
 
   // console.log(singleAssessment);
 const handleNextQuestion = () =>{
   setQuestion(question + 1)
 }
+
+const handleCorrectAnswer = () => {
+  setAnswer(answer)
+}
+
+
 const activeQuestion = singleAssessment.questions[question - 1]
+
   return (
     <div className="menu">
       <div>{singleAssessment.difficulty}</div>
       <div>{singleAssessment.name}</div>
-
       <div>
      
         <div dangerouslySetInnerHTML={{__html:activeQuestion.description.html}}/>
-       <div>{activeQuestion.options}</div>
+       <div>{activeQuestion.options.map(answer => {
+         return(
+         <div><button onClick={handleCorrectAnswer}>{answer}</button></div>
+         )
+       })}</div>
         <button onClick={handleNextQuestion}>Next</button>
       </div>
 
